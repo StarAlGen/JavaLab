13c18291bd330f1df64b350dce50ed9e75ee2f4c
Алексей Стародубов <alekseistarodubov1@yandex.ru>
2024-09-30 16:51:37 +0300

    Wrote the AtmService interface and the AtmServiceImpl class that implements it

--- src/main/java/tech/reliab/course/starodubovLab/bank/service/AtmService.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/AtmService.java
@@ -1,4 +1,13 @@
 package tech.reliab.course.starodubovLab.bank.service;
 
+import tech.reliab.course.starodubovLab.bank.entity.BankAtm;
+
+import java.math.BigDecimal;
+
 public interface AtmService {
+    BankAtm create (BankAtm bankAtm);
+
+    boolean depositMoney(BankAtm bankAtm, BigDecimal amount);
+
+    boolean withdrawMoney(BankAtm bankAtm, BigDecimal amount);
 }
--- src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/AtmServiceImpl.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/AtmServiceImpl.java
@@ -1,4 +1,68 @@
 package tech.reliab.course.starodubovLab.bank.service.impl;
 
-public class AtmServiceImpl {
+import tech.reliab.course.starodubovLab.bank.entity.BankAtm;
+import tech.reliab.course.starodubovLab.bank.service.AtmService;
+
+import java.math.BigDecimal;
+
+public class AtmServiceImpl implements AtmService {
+
+    @Override
+    public BankAtm create(BankAtm bankAtm){
+        if (bankAtm == null){
+            return null;
+        }
+        if (bankAtm.getTotalMoney().signum() < 0){
+            System.err.println("Error: cannot create BankAtm - totalMoney must be non-negative");
+            return null;
+        }
+        if (bankAtm.getMaintenanceCost().signum() < 0){
+            System.err.println("\"Error: cannot create BankAtm - maintenanceCost must be non-negative");
+            return null;
+        }
+        if (bankAtm.getBankOffice() == null){
+            System.err.println("\"Error: cannot create BankAtm - bankOffice cannot be null");
+            return null;
+        }
+        return new BankAtm(bankAtm);
+    }
+
+    @Override
+    public boolean depositMoney(BankAtm bankAtm, BigDecimal amount){
+        if (bankAtm == null){
+            System.err.println("\"Error: BankAtm cannot deposit money - non existing ATM");
+            return false;
+        }
+        if (amount.signum() < 0){
+            System.err.println("Error: BankAtm cannot deposit money - amount is not positive");
+        }
+        if (!bankAtm.getCashDepositAvailable()){
+            System.err.println("Error: BankAtm cannot deposit money - deposit is not allowed");
+            return false;
+        }
+        bankAtm.setTotalMoney(bankAtm.getTotalMoney().add(amount));
+        return true;
+    }
+
+    @Override
+    public boolean withdrawMoney(BankAtm bankAtm, BigDecimal amount){
+        if (bankAtm == null){
+            System.err.println("Error: BankAtm cannot withdraw money - non existing ATM");
+            return false;
+        }
+        if (amount.signum() < 0){
+            System.err.println("Error: BankAtm cannot withdraw money - amount is not positive");
+            return false;
+        }
+        if (!bankAtm.getCashWithdrawalAvailable()){
+            System.err.println("Error: BankAtm cannot withdraw money - deposit is not allowed");
+            return false;
+        }
+        if (bankAtm.getTotalMoney().compareTo(amount) < 0){
+            System.err.println("Error: BankAtm cannot withdraw money - ATM does not have enough money");
+            return false;
+        }
+        bankAtm.setTotalMoney(bankAtm.getTotalMoney().subtract(amount));
+        return true;
+    }
 }
