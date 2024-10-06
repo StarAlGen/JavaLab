1e8f6bcfc03f4b73454c2ffeb38f5c6a44682061
Алексей Стародубов <alekseistarodubov1@yandex.ru>
2024-10-06 13:00:54 +0300

    implements employee interface

--- src/main/java/tech/reliab/course/starodubovLab/bank/service/EmployeeService.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/EmployeeService.java
@@ -1,4 +1,10 @@
 package tech.reliab.course.starodubovLab.bank.service;
 
+import tech.reliab.course.starodubovLab.bank.entity.BankOffice;
+import tech.reliab.course.starodubovLab.bank.entity.Employee;
+
 public interface EmployeeService {
+    Employee create(Employee employee);
+
+    boolean transferEmployee(Employee employee, BankOffice bankOffice);
 }
--- src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/EmployeeServiceImpl.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,44 @@
 package tech.reliab.course.starodubovLab.bank.service.impl;
 
-public class EmployeeServiceImpl {
+import tech.reliab.course.starodubovLab.bank.entity.BankOffice;
+import tech.reliab.course.starodubovLab.bank.entity.Employee;
+import tech.reliab.course.starodubovLab.bank.service.EmployeeService;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Override
+    public Employee create(Employee employee){
+        if (employee == null){
+            return null;
+        }
+        if (employee.getBank() == null){
+            System.err.println("Error: Employee - no bank");
+            return null;
+        }
+        if (employee.getBankOffice() == null){
+            System.err.println("Error: Employee - no office");
+            return null;
+        }
+        if (employee.getSalaryAmount().signum() <= 0){
+            System.err.println("Error: Employee - salary should not be negative or equal to zero");
+            return null;
+        }
+        return new Employee(employee);
+    }
+
+    @Override
+    public boolean transferEmployee(Employee employee, BankOffice bankOffice){
+        if (employee != null && bankOffice != null){
+            if (employee.getBank() == bankOffice.getBank()){
+                employee.setBankOffice(bankOffice);
+            }
+            else{
+                employee.getBank().setEmployeeCount(employee.getBank().getEmployeeCount() - 1);
+                employee.setBank(bankOffice.getBank());
+                employee.setBankOffice(bankOffice);
+            }
+            return true;
+        }
+        return false;
+    }
 }
