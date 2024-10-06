7713c6e3bbce6ab81d3b23af9e0056c318255220
Алексей Стародубов <alekseistarodubov1@yandex.ru>
2024-10-06 12:36:43 +0300

    implements CreditAccount interface

--- src/main/java/tech/reliab/course/starodubovLab/bank/service/CreditAccountService.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/CreditAccountService.java
@@ -1,4 +1,9 @@
 package tech.reliab.course.starodubovLab.bank.service;
 
+import tech.reliab.course.starodubovLab.bank.entity.CreditAccount;
+
 public interface CreditAccountService {
+    CreditAccount create (CreditAccount creditAccount);
+
+    boolean makeMonthlyPayment(CreditAccount creditAccount);
 }
--- src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/CreditAccountServiceImpl.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/CreditAccountServiceImpl.java
@@ -1,4 +1,47 @@
 package tech.reliab.course.starodubovLab.bank.service.impl;
 
-public class CreditAccountServiceImpl {
+import tech.reliab.course.starodubovLab.bank.entity.CreditAccount;
+import tech.reliab.course.starodubovLab.bank.service.CreditAccountService;
+
+import java.math.BigDecimal;
+
+public class CreditAccountServiceImpl implements CreditAccountService {
+    public CreditAccount create(CreditAccount creditAccount){
+        if (creditAccount == null){
+            return null;
+        }
+        if (creditAccount.getBank() == null){
+            System.err.println("Error: CreditAccount - there must be a bank in which this account is registered ");
+            return null;
+        }
+        if (creditAccount.getUser() == null){
+            System.err.println("Error: CreditAccount - does not have a user");
+            return null;
+        }
+        if (creditAccount.getMonthCount() < 1){
+            System.err.println("Error: CreditAccount - the number of months must be at least 1");
+            return null;
+        }
+        if (creditAccount.getCreditAmount().signum() <= 0){
+            System.err.println("Error: CreditAccount - credit amount must be positive");
+            return null;
+        }
+        return new CreditAccount(creditAccount);
+    }
+
+    public boolean makeMonthlyPayment(CreditAccount creditAccount){
+        if (creditAccount == null || creditAccount.getPaymentAccount() == null){
+            System.err.println("Error: CreditAccount - no account to take money from");
+            return false;
+        }
+        final BigDecimal monthlyPayment = creditAccount.getMonthlyPayment();
+        final BigDecimal paymentAccountBalance = creditAccount.getPaymentAccount().getBalance();
+        if (paymentAccountBalance.compareTo(monthlyPayment) < 0){
+            System.err.println("Error: CreditAccount - insufficient funds for monthly payment");
+            return false;
+        }
+        creditAccount.setRemainingCreditAmount(creditAccount.getRemainingCreditAmount().subtract(monthlyPayment));
+        creditAccount.getPaymentAccount().setBalance(paymentAccountBalance.subtract(monthlyPayment));
+        return true;
+    }
 }
