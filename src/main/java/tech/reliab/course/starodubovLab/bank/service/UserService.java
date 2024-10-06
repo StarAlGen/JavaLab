64677bacf0b8ab653c6239465bf21efde16fe93b
Алексей Стародубов <alekseistarodubov1@yandex.ru>
2024-10-06 13:52:13 +0300

    implements User interface

--- src/main/java/tech/reliab/course/starodubovLab/bank/service/UserService.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/UserService.java
@@ -1,4 +1,10 @@
 package tech.reliab.course.starodubovLab.bank.service;
 
+import tech.reliab.course.starodubovLab.bank.entity.Bank;
+import tech.reliab.course.starodubovLab.bank.entity.User;
+
 public interface UserService {
+    User create(User user);
+
+    boolean changeBank(User user, Bank bank);
 }
--- src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/UserServiceImpl.java
+++ src/main/java/tech/reliab/course/starodubovLab/bank/service/impl/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package tech.reliab.course.starodubovLab.bank.service.impl;
 
-public class UserServiceImpl {
+import tech.reliab.course.starodubovLab.bank.entity.Bank;
+import tech.reliab.course.starodubovLab.bank.entity.User;
+import tech.reliab.course.starodubovLab.bank.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User create(User user){
+        if  (user == null){
+            return null;
+        }
+        if(user.getBank() == null){
+            System.err.println("Error: User - must have bank");
+            return null;
+        }
+        return new User(user);
+    }
+
+    @Override
+    public boolean changeBank(User user, Bank bank){
+        if (user != null && bank != null){
+            if (user.getBank() == bank){
+                return false;
+            }
+            user.getBank().setUserCount(user.getBank().getUserCount() - 1);
+            user.setBank(bank);
+            return true;
+        }
+        return false;
+    }
 }
